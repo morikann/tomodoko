e39dda0950385739518bd056453114082ee449cf
morikann <mrknt0088@gmail.com>
2021-10-28 11:38:38 +0900

    アカウント登録画面の作成

--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import '../component/common_button.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  late String username;
+  late String email;
+  late String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('アカウント登録'),
+          centerTitle: true,
+          title: const Text(
+            'アカウント登録',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30.0),
@@ -19,11 +35,77 @@ class SignupScreen extends StatelessWidget {
                 tag: 'logo',
                 child: SizedBox(
                   child: Image.asset('images/tomodoko_top.png'),
-                  height: 300,
+                  height: 250,
                 ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              CommonTextField(
+                name: 'ユーザー名',
+                onChanged: (value) {
+                  username = value;
+                },
+              ),
+              CommonTextField(
+                name: 'メールアドレス',
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              CommonTextField(
+                name: 'パスワード',
+                obscure: true,
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              const SizedBox(height: 30),
+              CommonButton(
+                name: '登録',
+                onPressed: () {
+                  print(username);
+                  print(email);
+                  print(password);
+                },
+                backgroundColor: Colors.purple,
+                textColor: Colors.white,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('ログインはこちら'),
               ),
             ],
           ),
         ));
+  }
+}
+
+class CommonTextField extends StatelessWidget {
+  final String name;
+  final bool? obscure;
+  final Function(String) onChanged;
+
+  const CommonTextField({
+    required this.name,
+    this.obscure,
+    required this.onChanged,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: onChanged,
+      obscureText: obscure ?? false,
+      decoration: InputDecoration(
+        label: Text(
+          name,
+          style: const TextStyle(
+            fontSize: 14,
+          ),
+        ),
+      ),
+    );
   }
 }
