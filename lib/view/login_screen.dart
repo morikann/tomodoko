c88f132fd1f139d0ae6661246d4cfb172ccbfc32
morikann <mrknt0088@gmail.com>
2021-10-31 17:02:06 +0900

    ログイン処理中にspinnerを表示

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -4,6 +4,7 @@ import '../component/common_button.dart';
 import '../component/common_text_field.dart';
 import 'signup_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -14,9 +15,28 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _showSpinner = false;
   final _auth = FirebaseAuth.instance;
   late String email = '';
   late String password = '';
+
+  Future<void> signin(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Navigator.of(context).pushNamedAndRemoveUntil(
+        UserListScreen.id,
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,68 +50,64 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Flexible(
-              child: Hero(
-                tag: 'logo',
-                child: SizedBox(
-                  child: Image.asset('images/tomodoko_top.png'),
-                  height: 200,
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: SizedBox(
+                      child: Image.asset('images/tomodoko_top.png'),
+                      height: 200,
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(
+                  height: 40,
+                ),
+                CommonTextField(
+                  label: 'メールアドレス',
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                CommonTextField(
+                  label: 'パスワード',
+                  obscure: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                ),
+                const SizedBox(height: 30),
+                CommonButton(
+                  name: 'ログイン',
+                  onPressed: () async {
+                    setState(() {
+                      _showSpinner = true;
+                    });
+                    await signin(email, password);
+                    setState(() {
+                      _showSpinner = false;
+                    });
+                  },
+                  backgroundColor: Colors.purple,
+                  textColor: Colors.white,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(SignupScreen.id);
+                  },
+                  child: const Text('アカウント登録はこちら'),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 40,
-            ),
-            CommonTextField(
-              label: 'メールアドレス',
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            CommonTextField(
-              label: 'パスワード',
-              obscure: true,
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            const SizedBox(height: 30),
-            CommonButton(
-              name: 'ログイン',
-              onPressed: () async {
-                try {
-                  await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    UserListScreen.id,
-                    (route) => false,
-                  );
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found') {
-                    print('No user found for that email.');
-                  } else if (e.code == 'wrong-password') {
-                    print('Wrong password provided for that user.');
-                  }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              backgroundColor: Colors.purple,
-              textColor: Colors.white,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(SignupScreen.id);
-              },
-              child: const Text('アカウント登録はこちら'),
-            ),
-          ],
+          ),
         ),
       ),
     );
