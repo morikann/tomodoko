54d867e8b99243dd636e1ffd0c2d4b07e90969e7
morikann <mrknt0088@gmail.com>
2021-11-19 18:14:32 +0900

    キーボード表示時に、他の要素をタップしたらキーボードを消す

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -80,113 +80,116 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: ModalProgressHUD(
         inAsyncCall: _showSpinner,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
-            child: SingleChildScrollView(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height - 100,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Hero(
-                        tag: 'logo',
-                        child: SizedBox(
-                          child: Image.asset('images/tomodoko_top.png'),
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height - 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Hero(
+                          tag: 'logo',
+                          child: SizedBox(
+                            child: Image.asset('images/tomodoko_top.png'),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              keyboardType: TextInputType.emailAddress,
-                              validator: (value) {
-                                // 1文字以上必要
-                                if (value == null || value.isEmpty) {
-                                  return 'メールアドレスを入力してください';
-                                }
-                                // メールアドレス以外は受けつけない
-                                if (!EmailValidator.validate(value)) {
-                                  return '正しいメールアドレスを入力してください';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                setState(() {
-                                  email = value!;
-                                });
-                              },
-                              controller: _emailController,
-                              decoration: const InputDecoration(
-                                helperText: '*必須',
-                                helperStyle: TextStyle(
-                                  color: Colors.red,
-                                ),
-                                labelText: 'メールアドレス',
-                              ),
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              onSaved: (value) {
-                                setState(() {
-                                  password = value!;
-                                });
-                              },
-                              controller: _passwordController,
-                              decoration: const InputDecoration(
-                                helperText: '*必須',
-                                helperStyle: TextStyle(
-                                  color: Colors.red,
-                                ),
-                                labelText: 'パスワード',
-                              ),
-                            ),
-                            const SizedBox(height: 30),
-                            CommonButton(
-                              name: 'ログイン',
-                              onPressed: () async {
-                                if (_formKey.currentState!.validate()) {
-                                  setState(() {
-                                    _showSpinner = true;
-                                  });
-                                  _formKey.currentState!.save();
-                                  await signIn(email, password);
-                                  setState(() {
-                                    _showSpinner = false;
-                                  });
-                                }
-                              },
-                              backgroundColor: Colors.blue,
-                              textColor: Colors.white,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8),
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context)
-                                      .pushNamed(SignupScreen.id);
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                validator: (value) {
+                                  // 1文字以上必要
+                                  if (value == null || value.isEmpty) {
+                                    return 'メールアドレスを入力してください';
+                                  }
+                                  // メールアドレス以外は受けつけない
+                                  if (!EmailValidator.validate(value)) {
+                                    return '正しいメールアドレスを入力してください';
+                                  }
+                                  return null;
                                 },
-                                child: const Text(
-                                  'アカウント登録はこちら',
-                                  style: TextStyle(color: Colors.blue),
+                                onSaved: (value) {
+                                  setState(() {
+                                    email = value!;
+                                  });
+                                },
+                                controller: _emailController,
+                                decoration: const InputDecoration(
+                                  helperText: '*必須',
+                                  helperStyle: TextStyle(
+                                    color: Colors.red,
+                                  ),
+                                  labelText: 'メールアドレス',
                                 ),
                               ),
-                            ),
-                          ],
+                              TextFormField(
+                                obscureText: true,
+                                onSaved: (value) {
+                                  setState(() {
+                                    password = value!;
+                                  });
+                                },
+                                controller: _passwordController,
+                                decoration: const InputDecoration(
+                                  helperText: '*必須',
+                                  helperStyle: TextStyle(
+                                    color: Colors.red,
+                                  ),
+                                  labelText: 'パスワード',
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              CommonButton(
+                                name: 'ログイン',
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    setState(() {
+                                      _showSpinner = true;
+                                    });
+                                    _formKey.currentState!.save();
+                                    await signIn(email, password);
+                                    setState(() {
+                                      _showSpinner = false;
+                                    });
+                                  }
+                                },
+                                backgroundColor: Colors.blue,
+                                textColor: Colors.white,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed(SignupScreen.id);
+                                  },
+                                  child: const Text(
+                                    'アカウント登録はこちら',
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
